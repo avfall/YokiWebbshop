9af90b6e288d84e47129401715743a01591a9206
bbqblaster <dean@apakossa.org>
2011-03-01 16:58:26 +0100

    added customer and staff

---
+++ src/org/apakossa/server/Customer.java
@@ -0,0 +1,13 @@
+package org.apakossa.server;
+
+public class Customer extends User {
+
+	public Customer(String email, String password, String firstname,
+			String lastname, String adress, String postalCode, String city,
+			String country) {
+		super(email, password, firstname, lastname, adress, postalCode, city,
+				country);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/org/apakossa/server/Staff.java
@@ -0,0 +1,12 @@
+package org.apakossa.server;
+
+public class Staff extends User {
+
+	public Staff(String email, String password, String firstname,
+			String lastname, String adress, String postalCode, String city,
+			String country) {
+		super(email, password, firstname, lastname, adress, postalCode, city, country);
+		// TODO Auto-generated constructor stub
+	}
+
+}
